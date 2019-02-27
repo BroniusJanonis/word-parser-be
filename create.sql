c9b8914a7920e663cefa7ff4ab2f3b4f6a98874b
Bronius <Bronius.Janonis@inntec.lt>
2019-02-27 22:01:11 +0200

    added improved gitignore

--- create.sql
+++ create.sql
@@ -1,1 +1,1 @@
-create table file_upload (id varchar(255) not null, content_type varchar(255), file_content blob, file_name varchar(255), uploaded_on timestamp, primary key (id))
+create table file_upload (id varchar(255) not null, content_type varchar(255), file_content oid, file_name varchar(255), uploaded_on timestamp, primary key (id))
